a0428f01c4a58af0d27e5a11cee274e287487913
BinhHung <binhhung2399@gmail.com>
2023-09-06 22:10:32 +0700

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import{CallIcon, DocumentIcon} from "./svg";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//Pages 
+import Home from "./pages/home";  
+import Login from "./pages/login";  
+import Register from "./pages/register";  
+
 
 function App() {
   return (
     <div className="dark">
-    </div> 
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 export default App;
