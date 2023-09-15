08a2a6405a0f6f3c5d8edb7ffeb0f7c2dec4e6b3
BinhHung <binhhung2399@gmail.com>
2023-09-15 17:16:30 +0700

    Finishing sign up form markup and styles

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -2,8 +2,12 @@ import { useForm } from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import { signUpSchema } from "../../utils/validation";
 import AuthInput from "./AuthInput";
+import { useSelector } from "react-redux";
+import  PulseLoader  from "react-spinners/PulseLoader";
+import { Link } from "react-router-dom";
 
 export default function RegisterForm(){
+    const {status} = useSelector((state) => state.user);
     const {
         register, 
         handleSubmit, 
@@ -52,7 +56,21 @@ export default function RegisterForm(){
                     register={register}
                     error={errors?.password?.message}
                     />
-                    <button type="submit">submit</button>
+                    {/*Submit button */}
+                    <button
+                    className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide
+                    font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
+                    type="submit">
+                        {status == "loading" ? <PulseLoader color= "#fff" size={16}/> : "Sign up"}
+                    </button>
+                    {/* Sign in link*/}
+                    <p className="flex flex-col items-center justify-center mt-10 text-center text-md dark:text-dark_text_1">
+                        <span>Have an account?</span>
+                        <Link href="/login"
+                             className=" hover:underline cursor-pointer transition ease-in duration-300">
+                            Sign in
+                        </Link>
+                    </p>
                 </form>
             </div>        
         </div>
